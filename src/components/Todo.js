200f7d2d03b3c8712d80df66e91a35ff1db6abfc
Van Doan <vandoan@vans-mbp.mynetworksettings.com>
2023-09-28 16:27:48 -0700

    ADM 0129 - Add updates and function before deleting files and starting new project

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,27 +1,31 @@
-import { useState } from 'react';
-import Modal from './Modal';
-import Backdrop from './Backdrop';
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
 
 function Todo(props) {
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-    function deleteHandler() {
-        setModalIsOpen(true)
-        console.log('Clicked.');
-    }
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  function deleteHandler() {
+    setModalIsOpen(true);
+    console.log("Clicked.");
+  }
 
-    function closeModalHandler() {
-        setModalIsOpen(false);
-    }
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
 
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div>
-        <button className="btn" onClick={deleteHandler}>Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
       {/* { ModalIsOpen ? <Modal /> : null } */}
-      { modalIsOpen && <Modal /> }
-      { modalIsOpen && <Backdrop onCancel={closeModalHandler}/> }
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfi x={closeModalHandler} />
+      )}
+      {modalIsOpen && (<Backdrop onCancel={closeModalHandler} />)}
     </div>
   );
 }
