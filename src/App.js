962707f4f6fc9af5e2cc8aca229bbeea8e959bbd
Van Doan <vandoan@vans-mbp.mynetworksettings.com>
2023-09-27 21:19:30 -0700

    ADM 1:23 - Add styling sheet, add logic and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Todo from './components/Todo';
 
 function App() {
   return (
-    <div>Hello, world</div>
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React' />
+      <Todo text='Master React' />
+      <Todo text='Explore the full React course' />
+    </div>
   );
 }
 
